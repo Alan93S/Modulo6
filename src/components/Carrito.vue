91532fc6373de4d291ab639085e70465b29796b7
manuelherradaM <manuel.musri@gmail.com>
2022-05-17 20:36:00 -0400

    carrito modal

--- src/components/Carrito.vue
+++ src/components/Carrito.vue
@@ -1,30 +1,47 @@
 <template>
-    <div class="my-5">
-        <h4>Carrito de compras</h4>
-        <table class="table">
-            <thead>
-            <tr>
+   <div v-if="showModal">
+    <transition name="modal">
+      <div class="modal-mask">
+        <div class="modal-wrapper">
+
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Modal title</h5>
+                    <tr>
                 <th scope="col">#</th>
                 <th scope="col">Item</th>
                 <th scope="col">Cantidad</th>
                 <th scope="col">Imagen</th>
                 <th scope="col">Total</th>
             </tr>
-            </thead>
-            <tbody id="items">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true" @click="showModal = false">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                   <tbody id="items">
                 <Items
                     v-for="item in carrito" :key="item.id"
                     :item="item"
                 />
             </tbody>
-            <tfoot>
-            <tr id="footer-carrito">
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" @click="showModal = false">Close</button>
+                    <tr id="footer-carrito">
                 <th scope="row" colspan="5" v-if="Object.keys(carrito).length === 0">Carrito vacío - comience a comprar!</th>
                 <Footer v-else/>
             </tr>
-            </tfoot>
-        </table>
-    </div>
+                </div>
+            </div>
+        </div>
+
+        </div>
+      </div>
+    </transition>
+  </div>
+  <button @click="showModal = true">Click</button>
 </template>
 
 <script>
@@ -39,6 +56,30 @@ export default {
         const carrito = computed(() => store.state.carrito)
 
         return {carrito}
+    },
+    data(){
+        return{
+            showModal :false
+        }
     }
 }
 </script>
+<style>
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, .5);
+  display: table;
+  transition: opacity .3s ease;
+}
+
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+</style>
